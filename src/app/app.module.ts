3e8575575fc37339da29993b8f97ba3f9496dbbf
4ReeX <56444853+4ReeX@users.noreply.github.com>
2021-12-03 10:40:36 +0300

    create new component's header, header-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header-menu/header-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-menu',
+  templateUrl: './header-menu.component.pug',
+  styleUrls: ['./header-menu.component.sass']
+})
+export class HeaderMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
