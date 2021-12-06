0381b390c8b34d1d2895f2333a030bbd3167f801
4ReeX <56444853+4ReeX@users.noreply.github.com>
2021-12-06 18:17:20 +0300

    06.12.2021

--- src/app/video-bg/video-bg.component.ts
+++ src/app/video-bg/video-bg.component.ts
@@ -10,19 +10,16 @@ import {Component, OnInit} from '@angular/core';
 
 export class VideoBgComponent implements OnInit {
     public images = [
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
-    {path: 'assets/background/bg1.jpg'},
+    {path: 'assets/images/content/1.jpg'},
+    {path: 'assets/images/content/2.jpg'},
+    {path: 'assets/images/content/3.jpg'},
+    {path: 'assets/images/content/4.jpg'},
+    {path: 'assets/images/content/5.jpg'},
+    {path: 'assets/images/content/6.jpg'},
+    {path: 'assets/images/content/7.jpg'},
+    {path: 'assets/images/content/8.jpg'},
+    {path: 'assets/images/content/9.jpg'},
+    {path: 'assets/images/content/10.jpg'}
   ]
   ngOnInit(): void {
   }
